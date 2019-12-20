4d09a76ae6a72e666009d6b751dff2198dd1ebf6
Quang Nguyen <quangn2595@gmail.com>
2019-12-20 14:59:03 -0800

    initialize spring boot project with spring initializr

---
+++ src/main/java/com/quangnguyen/zapverr/ZapverrApplication.java
@@ -0,0 +1,13 @@
+package com.quangnguyen.zapverr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZapverrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZapverrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/quangnguyen/zapverr/ZapverrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quangnguyen.zapverr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZapverrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
