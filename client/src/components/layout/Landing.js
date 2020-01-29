a90b789fe791091233f84b369ca2e55d4cef80be
Quang Nguyen <quangn2595@gmail.com>
2020-01-29 12:13:09 -0800

    use bootstrap to create better looking login and register forms

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -5,119 +5,114 @@ const Landing = () => {
   return (
     <section className="landing">
       <div className="landing-info">
-        {/* <Tab.Container className="loginForm">
-          <Row>
-            <Col>
-              <Nav>
-                <Nav.Item>
-
-                </Nav.Item>
-              </Nav>
-            </Col>
-          </Row>
-        </Tab.Container> */}
-        <div class="card home text-center card-form">
-          <div class="card-body">
-            <ul class="nav nav-tabs md-tabs tabs-2 darken-3" role="tablist">
-              <li class="nav-item">
+        <div className="card home text-center card-form">
+          <div className="card-body">
+            <ul className="nav nav-tabs md-tabs tabs-2 darken-3" role="tablist">
+              <li className="nav-item">
                 <a
-                  class="nav-link active text-dark"
+                  className="nav-link active text-dark"
                   data-toggle="tab"
-                  href="#panel7"
+                  href="#login"
                   role="tab"
                 >
-                  <i class="fas fa-user mr-1"></i>
+                  <i className="fas fa-user mr-1"></i>
                   Login
                 </a>
               </li>
-              <li class="nav-item">
+              <li className="nav-item">
                 <a
-                  class="nav-link text-dark"
+                  className="nav-link text-dark"
                   data-toggle="tab"
-                  href="#panel8"
+                  href="#register"
                   role="tab"
                 >
-                  <i class="fas fa-user-plus mr-1"></i>
+                  <i className="fas fa-user-plus mr-1"></i>
                   Register
                 </a>
               </li>
             </ul>
-            <div class="tab-content">
+            <div className="tab-content">
               <div
-                class="tab-pane fade in show active"
-                id="panel7"
+                className="tab-pane fade in show active"
+                id="login"
                 role="tabpanel"
               >
-                <h3 class="mt-3">Welcome Back</h3>
-                <p>Log into your account</p>
-                <form action="/" method="POST" class="md-form form-sm">
+                <h3 className="mt-3">Welcome Back</h3>
+                <p className="mt-3 mb-3">Log into your account</p>
+                <form action="/" method="POST" className="md-form form-sm">
                   <input
                     required
                     type="text"
                     name="username"
                     placeholder="Email"
-                    class="form-control form-control-lg my-2"
+                    className="form-control form-control-lg my-2"
                   />
                   <input
                     required
                     name="password"
                     type="password"
                     placeholder="Password"
-                    class="form-control form-control-lg my-2"
+                    className="form-control form-control-lg my-2"
                   />
                   <input
                     type="submit"
                     value="Log In"
-                    class="btn btn-outline-light btn-block mt-4"
+                    className="btn btn-outline-light btn-block mt-4"
                   />
                 </form>
               </div>
-              <div class="tab-pane fade" id="panel8" role="tabpanel">
-                <h3 class="mt-3">Sign Up Today</h3>
-                <p>Please fill out this form to register</p>
-                <form action="/signup" method="POST" class="md-form form-sm">
-                  <div class="form-group">
+              <div className="tab-pane fade" id="register" role="tabpanel">
+                <h3 className="mt-3">Sign Up Today</h3>
+                <p className="mt-3 mb-3">
+                  Please fill out this form to register
+                </p>
+                <form
+                  action="/signup"
+                  method="POST"
+                  className="md-form form-sm"
+                >
+                  <div className="form-group">
                     <input
                       required
                       type="text"
                       name="firstName"
                       placeholder="First Name"
-                      class="form-control form-control-lg my-2"
+                      className="form-control form-control-lg my-2"
                     />
                     <input
                       required
                       type="text"
                       name="lastName"
                       placeholder="Last Name"
-                      class="form-control form-control-lg my-2"
+                      className="form-control form-control-lg my-2"
                     />
                     <input
                       required
                       type="email"
                       name="email"
                       placeholder="Email"
-                      class="form-control form-control-lg my-2"
+                      className="form-control form-control-lg my-2"
                     />
                     <input
                       required
                       name="password"
                       type="password"
                       placeholder="Password"
-                      class="form-control form-control-lg my-2"
+                      className="form-control form-control-lg my-2"
                     />
                     <input
                       required
                       type="text"
                       name="phoneNumber"
                       placeholder="Mobile Number"
-                      class="form-control form-control-lg my-2"
+                      className="form-control form-control-lg my-2"
                     />
                     <input
                       type="submit"
                       value="Register"
                       data-toggle="modal"
                       data-target="#TermsAndAgreement"
-                      class="btn btn-outline-light btn-block mt-4"
+                      className="btn btn-outline-light btn-block mt-4"
                     />
                   </div>
                 </form>
@@ -126,6 +121,7 @@ const Landing = () => {
           </div>
         </div>
       </div>
+
       <div className="landing-background">
         <img
           src={landingBackground}
